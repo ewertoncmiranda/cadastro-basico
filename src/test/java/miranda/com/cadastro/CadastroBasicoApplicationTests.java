d8464de008ca4271fd2a179791dce09a647be0cf
ewertoncmiranda <sonardevplus@gmail.com>
2022-03-03 07:18:43 -0300

    configuracao inicial

---
+++ src/test/java/miranda/com/cadastro/CadastroBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package miranda.com.cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
